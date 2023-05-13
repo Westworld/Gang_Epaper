4888cc1bc3568adf35f031c6ad6088b85088c7fc
Westworld <Thomas.Maul@gmail.com>
2023-05-13 13:54:42 +0200

    vor icon

--- src/main.h
+++ src/main.h
@@ -4,3 +4,4 @@ void helloWorld(const char *HelloWorld);
 void UDBDebug(String message);
 void UDBDebug(const char * message);
 void MQTT_callback(char* topic, byte* payload, unsigned int length);
+String getMeteoconIcon(String icon);
